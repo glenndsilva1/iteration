b2a233a6da8b2d7e600cf96745e642f2cd74578c
glenndsilva1 <glenndsilva@rediffmail.com>
2024-05-28 14:57:46 +0530

    Multiply

---
+++ Multiply.java
@@ -0,0 +1,12 @@
+package iteration;
+
+public class Multiply {
+	private int number;
+	Multiply(int number){
+		this.number = number;
+	}
+	
+	public int value(int value){
+		return value * this.number;
+	}
+}
